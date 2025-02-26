9b3fc1593849c5550c70f2795e5cf80260a4efef
SimonLeo28 <simonleoalexanderpcmct2023@gmail.com>
2025-02-26 11:18:49 +0530

    Navbar changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <>
     <nav className="fixed top-0 left-0 w-full bg-indigo-800 bg-opacity-90 text-white p-4 shadow-lg z-50">
         <div className="container mx-auto flex justify-between items-center">
-          <h1 className="text-2xl font-bold">LifeSecure</h1>
+          <h1 className="text-2xl font-bold">Jeevan Saathi</h1>
           <ul className="flex gap-6">
             <li><Link to={"/"}><a className="hover:text-yellow-400">Home</a></Link></li>
             <li><Link to={"/Plans"}><a className="hover:text-yellow-400">Plans</a></Link></li>
